8bf0bc09d6584968b1e53c37f1ec787a9e416ddd
Kyohei Ito <je.suis.kyohei+github@gmail.com>
2024-10-24 18:21:43 +0900

    chore: can set build args

--- packages/override_pod_generator/lib/builder.dart
+++ packages/override_pod_generator/lib/builder.dart
@@ -1,4 +1,6 @@
 import 'package:build/build.dart';
 import 'package:override_pod_generator/src/override_pod_builder.dart';
 
-Builder overridePodsBuilder(BuilderOptions options) => OverridePodsBuilder();
+Builder overridePodsBuilder(BuilderOptions options) => OverridePodsBuilder(
+      options.config,
+    );
--- packages/override_pod_generator/lib/src/override_pod_builder.dart
+++ packages/override_pod_generator/lib/src/override_pod_builder.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:build/build.dart';
 import 'package:code_builder/code_builder.dart';
 import 'package:dart_style/dart_style.dart';
@@ -9,18 +7,29 @@ import 'package:path/path.dart';
 import 'package:source_gen/source_gen.dart';
 
 class OverridePodsBuilder implements Builder {
-  static const _outputName = 'override_pod.gen.dart';
+  const OverridePodsBuilder(this._config);
+
+  static const _defaultInput = 'lib/**.dart';
+  static const _defaultOutput = 'override_pod.gen.dart';
+
+  final Map<String, dynamic> _config;
+  String get _output => _config['output_file'] as String? ?? _defaultOutput;
 
   @override
   Map<String, List<String>> get buildExtensions {
-    return const {
-      r'$lib$': [_outputName],
+    return {
+      r'$lib$': [_output],
     };
   }
 
   @override
   Future<void> build(BuildStep buildStep) async {
-    final assets = buildStep.findAssets(Glob('lib/**/*.dart'));
+    final input = _config['input_file'] as String? ?? _defaultInput;
+    final annotation = _config['annotation_class'] as String?;
+    final valueName = _config['output_value_name'] as String? ??
+        '${buildStep.inputId.package}PodOverrides';
+
+    final assets = buildStep.findAssets(Glob(input));
     final allOverrides = <Expression>[];
 
     await for (final asset in assets) {
@@ -29,9 +38,11 @@ class OverridePodsBuilder implements Builder {
       }
 
       final library = await buildStep.resolver.libraryFor(asset);
-      final libraryOverrides = LibraryReader(library)
-          .annotatedWith(const TypeChecker.fromRuntime(OverridePod))
-          .map((annotated) {
+      final typeChecker = annotation != null
+          ? TypeChecker.fromUrl(annotation)
+          : const TypeChecker.fromRuntime(OverridePod);
+      final libraryOverrides =
+          LibraryReader(library).annotatedWith(typeChecker).map((annotated) {
         return refer(
           annotated.element.displayName,
           annotated.element.librarySource?.uri.toString(),
@@ -42,10 +53,9 @@ class OverridePodsBuilder implements Builder {
     }
 
     const reference = Reference('Override', 'package:riverpod/riverpod.dart');
-    final podOverridesField =
-        declareFinal('${buildStep.inputId.package}PodOverrides')
-            .assign(literalList(allOverrides, reference))
-            .statement;
+    final podOverridesField = declareFinal(valueName)
+        .assign(literalList(allOverrides, reference))
+        .statement;
 
     final overrideLibrary = Library(
       (b) => b..body.add(podOverridesField),
@@ -65,7 +75,7 @@ ${overrideLibrary.accept(emitter)}
     await buildStep.writeAsString(
       AssetId(
         buildStep.inputId.package,
-        join('lib', _outputName),
+        join('lib', _output),
       ),
       DartFormatter().format(content),
     );
